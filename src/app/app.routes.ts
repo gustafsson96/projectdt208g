f847d1aae12c289990d4182cda6b841f5b0de0de
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-30 21:09:44 +0200

    Create home and schedule components and their routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: 'home', pathMatch: 'full' },
+    { path: "home", component: HomeComponent}, 
+    { path: "schedule", component: ScheduleComponent}
+];
