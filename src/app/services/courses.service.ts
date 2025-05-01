fa80b39569e142a8b8e52d14d8257257b73dea3c
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-01 11:38:23 +0200

    Use httpClient for course service

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Course } from '../models/course';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  private url: string = '/miun_courses.json'
+
+  constructor(private http: HttpClient) { }
+
+  // Get courses
+  getCourses() : Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
 }
