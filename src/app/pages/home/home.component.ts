4445df6fd58f076e1600c6e1656971b5297273b7
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-01 11:35:30 +0200

    Import course table to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CourseTableComponent } from '../../course-table/course-table.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CourseTableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
