4bdfb7d37440d674d1a260bb18b7214e04357142
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-01 11:29:20 +0200

    Create separate component for course table

---
+++ src/app/course-table/course-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-table',
+  imports: [],
+  templateUrl: './course-table.component.html',
+  styleUrl: './course-table.component.css'
+})
+export class CourseTableComponent {
+
+}
