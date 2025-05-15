a8c363085b0303d84adf0c8bc9b7842adeabde01
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-15 20:30:27 +0200

    Create and style navigation bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const navLinks = document.querySelector('.nav-links') as HTMLUListElement;
+    const hamburgerMenu = document.querySelector('.hamburger-menu') as HTMLDivElement;
+    
+    if (navLinks) {
+      navLinks.classList.toggle('active', this.isMenuOpen);
+    }
+    if (hamburgerMenu) {
+      hamburgerMenu.classList.toggle('active', this.isMenuOpen);
+    }
+  }
 }
