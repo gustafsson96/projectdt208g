83dad65902bc25c2dd5e454fda579a0a90409dd2
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-16 12:54:33 +0200

    Implement site pagination for course list

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseService } from '../../services/course.service';
 import { Course } from '../../models/course';
@@ -14,7 +14,11 @@ import { FormsModule } from '@angular/forms';
 export class CoursesComponent {
   courses: Course[] = [];
   filteredCourses: Course[] = [];
+  paginatedCourses: Course[] = [];
   filteredValue: string = "";
+
+  currentPage = 1;
+  itemsPerPage = 10;
 
   constructor(private courseService : CourseService) {}
 
@@ -22,6 +26,7 @@ export class CoursesComponent {
     this.courseService.getCourses().subscribe((data) => {
       this.courses = data;
       this.filteredCourses = data;
+      this.updatePaginatedCourses();
     });
   }
 
@@ -31,5 +36,32 @@ export class CoursesComponent {
       course.courseName.toLowerCase().includes(filter) ||
       course.courseCode.toLowerCase().includes(filter)
     );
+    this.currentPage = 1;
+    this.updatePaginatedCourses();
+  }
+
+  // Site pagination
+  updatePaginatedCourses(): void {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedCourses = this.filteredCourses.slice(start, end);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePaginatedCourses();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginatedCourses();
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.filteredCourses.length / this.itemsPerPage);
   }
 }
